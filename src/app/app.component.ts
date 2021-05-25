cc06b5528690ac6aa284c9c0547064bf8a5aaff6
tylor-canton <tylor.canton@conduent.com>
2021-05-25 13:47:48 -0400

    Solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,47 @@
-import { Component, Inject } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { initialNotes } from './notes.fixture';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { NotesService } from './notes.service';
 import { NewNote, Note } from './note';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  selected?: Note|NewNote;
+export class AppComponent implements OnInit, OnDestroy {
+  selected?: Note | NewNote;
+  notesList: Note[];
+   constructor(private readonly notesService: NotesService) { }
+  public serviceSubscription: Subscription;
 
-  constructor(private readonly notesService: NotesService) {}
+  ngOnInit() {
+     this.serviceSubscription = this.notesService.notes$.subscribe(notes => {
+      this.notesList = notes;
+     });
 
-  selectNote(note: Note) {
-    // TODO: prevent changing original object
-    this.selected = note;
+  }
+  selectNote(note) {
+    const index = this.notesList.findIndex(currentNote => currentNote.id === note.id);
+    return this.selected = this.notesList[index];
+
   }
 
   newNote() {
     this.selected = createNewNote();
   }
 
-  saveNote(note: Note|NewNote) {
-    // TODO: save note
+  saveNote(note: Note) {
+    this.notesService.saveNote(note).subscribe(newData => {
+      console.log('New', newData);
+    });
+  }
+  ngOnDestroy() {
+    this.serviceSubscription.unsubscribe();
   }
 }
 
 function createNewNote(): NewNote {
-  return {title: '', body: '', color: '', favorite: false};
+  return { title: '', body: '', color: '', favorite: false };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { NotelistComponent } from './notes/notelist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotelistComponent
   ],
   imports: [
     BrowserModule,
